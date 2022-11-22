1559a6c3520a2c6d8f6b4d8aae64baeaad923406
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-11-22 18:05:18 +0200

    connected imageEditor route and errorHandler middleware to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,17 @@
 import express, { Express } from 'express';
 import cors from 'cors';
+import { errorsHandler } from '@middlewares/middlewares'
 import { ENV } from '@common/enums';
+
+import { imageEditor } from '@routes/routes'
 
 const app: Express = express();
 
-app
-    .use(cors({ origin: "*" }))
-    .use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.use(imageEditor);
+
+app.use(errorsHandler);
 
 app.listen(ENV.PORT, () => {
   console.log(`⚡️[server]:  Server is running at http://localhost:${ENV.PORT}`);
